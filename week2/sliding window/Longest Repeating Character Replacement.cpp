e020dafcc33efa5793d66313251befd2b1ef06a0
srishti_gupta <105485441+srishg@users.noreply.github.com>
2023-07-19 01:09:06 +0530

    Create Longest Repeating Character Replacement.cpp

---
+++ week2/sliding window/Longest Repeating Character Replacement.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int i=0,j=0,ans=0,cnt[26]={};
+        for(j=0;j<s.size();j++){
+            cnt[s[j]-'A']++;
+            while(j-i+1 - *max_element(cnt,cnt+26)>k){
+                cnt[s[i]-'A']--;
+                i++;
+            }
+            ans=max(ans,j-i+1);
+        }
+        
+        return ans;
+    }
+};
